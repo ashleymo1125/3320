d6cbcf04ace8a1f4faff4250770639866cbe719d
ashleymo1125 <67338461+ashleymo1125@users.noreply.github.com>
2020-07-22 19:49:00 -0500

    Update view_customers.php

--- hw8/view_customers.php
+++ hw8/view_customers.php
@@ -3,16 +3,16 @@
 $db = mysqli_connect('localhost','root','');
 mysqli_select_db($db ,'sakila');
 
-$query = "select customer.first_name, customer.last_name, address.address, city.city, address.district, address.postal_code, film.title 
+$query = "select customer.first_name, customer.last_name, address.address, city.city, address.district, address.postal_code, film.title
 from customer
 inner join 
 address, city, film, rental, inventory where (customer.customer_id = rental.customer_id
 and customer.address_id = address.address_id 
+and address.city_id = city.city_id 
 and rental.inventory_id = inventory.inventory_id 
 and inventory.film_id = film.film_id );";
 $results = mysqli_query($db, $query);
 
-echo print_r($results);
 ?>
 
 <!DOCTYPE html>
@@ -25,10 +25,13 @@ echo print_r($results);
     <th>City</th>
     <th>District</th>
     <th>Postal_Code</th>
-   <!-- <th>List of Films</th> -->
+    <th>List of Films</th>
     </tr>
     
   <?php  
+    
+    $films = array();
+    
     while($rows = mysqli_fetch_assoc($results))
     {
         echo "<tr>";
@@ -38,7 +41,18 @@ echo print_r($results);
         echo "<td>".$rows['city']."</td>";
         echo "<td>".$rows['district']."</td>";
         echo "<td>".$rows['postal_code']."</td>";
+        echo "<td>".$rows['title']."</td>";
+        $films = array("fistName" => $rows['first_name'],
+                       "lastName" => $rows['last_name'],
+                       "theAddress" => $rows['address'],
+                       "theCity" => $rows['city'],
+                       "district" => $rows['district'],
+                       "postalCode" => $rows['postal_code'],
+                       );
+        
+       
         echo "</tr>";
+       
     }      
     ?>
    
